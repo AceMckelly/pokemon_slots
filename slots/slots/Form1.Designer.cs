8c4be1a1454b2d4612ed547d701e02c10e3ef936
AceMckelly <66040412+AceMckelly@users.noreply.github.com>
2022-06-27 11:45:22 -0700

    Add files via upload

---
+++ slots/slots/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace slots
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 86);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(120, 13);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 85);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Location = new System.Drawing.Point(227, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 86);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(129, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Spin";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(292, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(224, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Coins";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(12, 13);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(317, 272);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 6;
+            this.pictureBox4.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 297);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox4);
+            this.Name = "Form1";
+            this.Text = "PokeSlots";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
---
+++ slots/slots/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace slots
+{
+    public partial class Form1 : Form
+    {
+        int coins;
+        public Form1()
+        {
+            InitializeComponent();
+            coins = 100;
+            pictureBox1.Image = Properties.Resources.ash_pikachu;
+            pictureBox2.Image = Properties.Resources.ash_pikachu;
+            pictureBox3.Image = Properties.Resources.ash_pikachu;
+            pictureBox4.Image = Properties.Resources.pokeball;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (coins < 10)
+            {
+                return;
+            }
+            Random r = new Random();
+            coins = coins - 10;
+            int num1 = r.Next(6);
+            int num2 = r.Next(6);
+            int num3 = r.Next(6);
+
+
+            switch (num1)
+            {
+                case 0:
+                    pictureBox1.Image = Properties.Resources.ash_pikachu;
+                    break;
+                case 1:
+                    pictureBox1.Image = Properties.Resources.butterfree; 
+                    break;
+                case 2:
+                    pictureBox1.Image = Properties.Resources.pidgeot;
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources.bulbasaur;
+                    break;
+                case 4:
+                    pictureBox1.Image = Properties.Resources.squirtle;
+                    break;
+                case 5:
+                    pictureBox1.Image = Properties.Resources.charizard;
+                    break;
+            }
+
+            switch (num2)
+            {
+                case 0:
+                    pictureBox2.Image = Properties.Resources.ash_pikachu;
+                    break;
+                case 1:
+                    pictureBox2.Image = Properties.Resources.butterfree;
+                    break;
+                case 2:
+                    pictureBox2.Image = Properties.Resources.pidgeot;
+                    break;
+                case 3:
+                    pictureBox2.Image = Properties.Resources.bulbasaur;
+                    break;
+                case 4:
+                    pictureBox2.Image = Properties.Resources.squirtle;
+                    break;
+                case 5:
+                    pictureBox2.Image = Properties.Resources.charizard;
+                    break;
+            }
+
+            switch (num3)
+            {
+                case 0:
+                    pictureBox3.Image = Properties.Resources.ash_pikachu;
+                    break;
+                case 1:
+                    pictureBox3.Image = Properties.Resources.butterfree;
+                    break;
+                case 2:
+                    pictureBox3.Image = Properties.Resources.pidgeot;
+                    break;
+                case 3:
+                    pictureBox3.Image = Properties.Resources.bulbasaur;
+                    break;
+                case 4:
+                    pictureBox3.Image = Properties.Resources.squirtle;
+                    break;
+                case 5:
+                    pictureBox3.Image = Properties.Resources.charizard;
+                    break;
+            }
+
+            if (num1 + num2 + num3 == 0)
+            {
+                coins = coins + 100;
+            }
+            else if(num1*num2+num1*num3+num2*num3==0)
+            {
+                coins = coins + 20;
+            }
+            else if (num1 * num2 * num3 == 0)
+            {
+                coins = coins + 10;
+            }
+
+            label1.Text = coins.ToString();
+            
+        }
+    }
+}
